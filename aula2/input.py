c305de1f8b0a9f731468d05aa9d04bc40d254119
Shallxd <ricardoffm04@gmail.com>
2024-09-28 15:28:35 +0100

    Add files via upload

---
+++ aula2/input.py
@@ -0,0 +1,6 @@
+n1 = input('Qual é o primeiro numero: ')
+n2 = input("Qual é o segundo numero:  ")
+
+media = n1 + n2/2
+
+print("este numero e", media)
---
+++ aula2/medias.py
@@ -0,0 +1,7 @@
+n1=int(input('Qual é o nr 1? '))
+n2=int(input('Qual é o nr 2? '))
+
+media = (n1+n2)/2
+
+print()
+print('A media do aluno é',media)
